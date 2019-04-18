441a6821d44574e37f8be3565e634a7299e40ce7
mateusz5564 <mateusz5564@gmail.com>
2019-04-19 01:27:32 +0200

    corrected a few shortcoming in models and the paths to the pictures have been arranged

--- forumapp/forum/admin.py
+++ forumapp/forum/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile, Post, Post_rating, Comment, Comment_rating
 
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Post_rating)
+admin.site.register(Comment_rating)
 # Register your models here.
--- forumapp/forum/models.py
+++ forumapp/forum/models.py
@@ -1,61 +1,74 @@
     
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+
+def user_path(instance, filename):
+    return 'avatar/%s/%s' % (instance.user.username, filename)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     bio = models.TextField(max_length=100, blank=True)
-    location = models.CharField(max_length=30)
-    avatar = models.ImageField(upload_to="avatars", default="default.png", blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    avatar = models.ImageField(upload_to=user_path, default="/avatar/default.jpg", blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 
 class Post(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.SET_NULL,
-        null = True,
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     text = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="images/%Y/%m/%D/")
+    image = models.ImageField(upload_to='post_images/%Y/%m/%d/')
     is_accepted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return "user: " + self.user.username + " | post_id: " + str(self.id)
+
 
 class Comment(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-    )
-    post = models.OneToOneField(
-        Post,
-        on_delete=models.CASCADE,
-    )
-    parent_id = models.ForeignKey('Comment', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    parent_id = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField(max_length=300)
 
+    def __str__(self):
+        result = "user: " + self.user.username + " | comment_id: " + str(self.id)
+        if self.parent_id:
+            result += " | parent_id: " + str(self.parent_id.id)
+        return result
+
 
 class Post_rating(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-    )
-    post = models.OneToOneField(
-        Post,
-        on_delete=models.CASCADE,
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "user: " + self.user.username + " | post_id: " + str(self.post.id)
 
 class Comment_rating(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-    )
-    comment = models.OneToOneField(
-        Comment,
-        on_delete=models.CASCADE,
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     is_positive = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "user: " + self.user.username + " | comment_id: " + str(self.comment.id)
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def post_save_receiver(sender, instance, **kwargs):
+    instance.profile.save()    
+
+
+        
+    
