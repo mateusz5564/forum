08f8236131e05c3eb367d29a2b9ab66aaa9ac849
mateusz5564 <mateusz5564@gmail.com>
2019-04-25 22:26:02 +0200

    add Vuetify to project and routing on the frontend side

--- forumapp/frontend/src/router.js
+++ forumapp/frontend/src/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { directives } from 'vuetify/lib';
+import Home from './views/Home.vue'
 
 Vue.use(Router);
 
@@ -7,5 +9,16 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: () => import('./views/Posts.vue')
+    }
   ],
 });
+
