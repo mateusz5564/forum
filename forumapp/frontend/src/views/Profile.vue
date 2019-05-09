8b34463f426293cd8600f9704e2f57d044fdea14
mateusz5564 <mateusz5564@gmail.com>
2019-05-10 00:10:32 +0200

    Profile view connected to the API, shows the data of current logged in user

--- forumapp/frontend/src/views/Profile.vue
+++ forumapp/frontend/src/views/Profile.vue
@@ -1,16 +1,37 @@
 <template>
   <v-container grey lighten-2>
     <v-layout>
-      <v-flex shrink>
+      <v-flex shrink mr-4>
         <v-avatar size="300px">
           <img :src="userAvatar" alt="avatar">
         </v-avatar>
       </v-flex>
-      <v-flex ml-4 grey lighten-1>
-        {{ username }}
-        <br>location
-        <br>bio
-        <br>
+      <v-flex pl-1 amber shrink></v-flex>
+      <v-flex ml-1>
+        <v-layout pt-3 pl-3 pb-1 display-1 font-weight-light>
+          {{ username }}
+        </v-layout>
+
+        <v-layout pl-3 pb-1>
+
+          <v-flex>
+            <v-icon>outlined_flag</v-icon>
+            {{ profile[0].user.date_joined}}
+          </v-flex>
+
+        </v-layout>
+
+        <v-layout pl-3 pb-3>
+          <v-flex>
+            <v-icon>location_on</v-icon>
+            {{ profile[0].location }}
+          </v-flex>
+        </v-layout>
+
+        <v-layout
+          pl-3
+          pb-1
+        >{{ profile[0].bio }} Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit iure dolorum accusamus iusto ut eveniet quidem debitis deleniti sunt et.</v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -37,9 +58,11 @@ export default {
     ])
   },
   mounted() {
-    axios.get(`${API}profiles/`).then(response => {
-      this.profile = response.data;
-    });
+    axios
+      .get(`${API}profiles/?user__username=${this.username}`)
+      .then(response => {
+        this.profile = response.data;
+      });
   }
 };
 </script>
